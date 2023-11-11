0d929ed1641885ee2835650b8f614c935b9c064c
Yana Lepska <lepskayana1624@gmail.com>
2023-11-11 18:42:57 +0200

    Code

---
+++ task-3.js
@@ -0,0 +1,9 @@
+function getElementWidth(content, padding, border) {
+    
+    const N = Number.parseFloat(content) + 2 * Number.parseFloat(padding) + 2 * Number.parseFloat(border);
+    return N;
+}
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(getElementWidth("351px", "13px", "2px")); // 381
